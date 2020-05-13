189902bf800bce7a769b074a3d596ee5b9bb9c2a
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-13 18:04:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
-# Make your shoe class here!
+class shoe
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def color
+    @color
+  end
+
+  def size
+    @size
+  end
+
+  def material
+    @material
+  end
+
+  def condition
+    @condition
+  end
+end
